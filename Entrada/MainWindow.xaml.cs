2f470375d8dfde6b502fd050bdd041126f6a3902
tonny <itonygame@gmail.com>
2019-04-05 12:30:02 -0700

    Detecta sonido mediante el microfono

--- Entrada/MainWindow.xaml.cs
+++ Entrada/MainWindow.xaml.cs
@@ -48,7 +48,35 @@ namespace Entrada
             waveIn.WaveFormat = new WaveFormat(44100, 16, 1);
             //BUFFER
             waveIn.BufferMilliseconds = 250;
-            //
+            //¿QUE HACER CUANDO HAY MUESRTAS DISPONIBLES?
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+
+            waveIn.StartRecording();
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            byte[] buffer = e.Buffer;
+            int bytesGrabados = e.BytesRecorded;
+            float acumulador = 0.0f;
+            for (int i = 0; i < bytesGrabados; i += 2)
+            {
+                //Transformando 2 bytes separados en una muestra de 16 bits
+                //1.- Toma el segundo byte y el antepone 8 0's L PRINCIPIO
+                //2.- Hace un "OR" con el primer byte, al cual automaticamente se le llenan 8 0's al final
+                short muestra = (short)(buffer[i + 1] << 8 | buffer[i]);
+                float muestra32Bits = (float)muestra / 32768.0f;
+                acumulador += Math.Abs(muestra32Bits);
+
+            }
+            float promedio = acumulador / (bytesGrabados / 2.0f);
+            sld_Microfono.Value = (double)promedio;
+
+        }
+
+        private void Btn_Detener_Click(object sender, RoutedEventArgs e)
+        {
+            waveIn.StopRecording();
         }
     }
 }
